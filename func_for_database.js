f1b1eebe1306450980cf5aaf9dd706ffc2199f48
aedwardsC <118393110+aedwardsC@users.noreply.github.com>
2024-09-05 20:22:54 -0700

    Work in progress server and database

---
+++ func_for_database.js
@@ -0,0 +1,11 @@
+function startDatabase(databaseConnection) {
+    // detect if there is a database already
+
+    // if there is...
+    // say that there is on the console
+
+    // if there isn't...
+    // create the database and the tables
+}
+
+module.exports = {startDatabase};
---
+++ server.js
@@ -0,0 +1,31 @@
+// Anne Edwards
+// CPSC 491-08
+// Two-Way Schedular
+
+// import the functions from func_for_database
+databaseFunctions = require("./func_for_database.js");
+
+// set up Node.js library
+let nodeJs = require("express");
+let program = nodeJs();
+
+// set up the library for parsing form data
+let dataParse = require("body-parser");
+// call the library for parsing the data
+program.use(dataParse.urlencoded({extend:true}));
+// for easily navigating between the forms
+program.use(express.static(__dirname + "/Company_forms"));
+
+
+// set up the database
+let mySql = require("mysql");
+let databaseConnection = mySql.createConnection({
+    host: "localhost",
+    user: "", // your username
+    password: "" // your password
+});
+
+startDatabase(databaseConnection);
+
+// listen on the port localhost:4000
+program.listen(4000);
